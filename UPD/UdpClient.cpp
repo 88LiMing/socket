80b802e051e797c2f7e488d979a25910afadb7c4
88LiMing <140676582+88LiMing@users.noreply.github.com>
2023-10-12 16:35:48 +0800

    Update UdpClient.cpp

--- UPD/UdpClient.cpp
+++ UPD/UdpClient.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8888
+
+int main() {
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Error in socket creation." << std::endl;
+        return -1;
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    char buffer[1024];
+    memset(buffer, 0, sizeof(buffer));
+
+    std::cout << "Enter a message to send: ";
+    std::cin.getline(buffer, sizeof(buffer));
+
+    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    close(sockfd);
+
+    return 0;
+}
